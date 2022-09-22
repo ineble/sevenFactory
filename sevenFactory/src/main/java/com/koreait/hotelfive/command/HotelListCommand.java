70073b43fecc861730e144a46d661f96ab3d9949
ineble <thdwltjq94@gmail.com>
2022-09-22 12:47:02 +0900

    semi_seven

---
+++ sevenFactory/src/main/java/com/koreait/hotelfive/command/HotelListCommand.java
@@ -0,0 +1,23 @@
+package com.koreait.hotelfive.command;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.ui.Model;
+
+import com.koreait.hotelfive.dao.HotelFiveDAO;
+
+public class HotelListCommand implements Command {
+
+	@Override
+	public void execute(SqlSession sqlSession, Model model) {
+		// TODO Auto-generated method stub
+
+		HotelFiveDAO hDAO = sqlSession.getMapper(HotelFiveDAO.class);
+		model.addAttribute("list2",hDAO.hotelList());
+		System.out.println(model);
+	}
+
+}
